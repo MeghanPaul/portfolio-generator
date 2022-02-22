d36202cb614b34b0c72c34354328c366bfe9af85
Meghan Paul <mlpaul4901@gmail.com>
2022-02-21 22:07:05 -0600

    generate an html file based on user command line input

---
+++ src/page-template.js
@@ -0,0 +1,19 @@
+const generatePage = (userName, gitHubName) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+    <body>
+        <h1>${userName}</h1>
+        <h2><a href="https://github.com/${gitHubName}">GitHub</a></h2>
+    </body>
+    </html>
+    `;
+};
+
+module.exports = generatePage;
