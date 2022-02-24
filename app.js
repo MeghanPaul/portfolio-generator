6579200b4d28a68ebfbff882f71de0362a57c96b
Meghan <mlpaul4901@gmail.com>
2022-02-24 17:37:33 -0600

    prepared template to handle all data from inquirer

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
-/*const fs = require('fs');
+const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
-const pageHTML = generatePage(userName,gitHubName);
+/*const pageHTML = generatePage(userName,gitHubName);
 
 fs.writeFile('./index.html',pageHTML, err => {
     if(err) throw new Error(err);
@@ -144,8 +144,65 @@ const promptProject = portfolioData => {
     );
 };
 
-promptUser()
+const mockData = {
+    
+    name: 'Meghan',
+    github: 'MeghanPaul',
+    confirmAbout: true,
+    about:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+    projects: [
+        {
+            name: 'Run Buddy',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['HTML', 'CSS'],
+            link: 'https://github.com/lernantino/run-buddy',
+            feature: true,
+            confirmAddProject: true
+        },
+        {
+            name: 'Taskinator',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['JavaScript', 'HTML', 'CSS'],
+            link: 'https://github.com/lernantino/taskinator',
+            feature: true,
+            confirmAddProject: true
+        },
+        {
+            name: 'Taskmaster Pro',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+            link: 'https://github.com/lernantino/taskmaster-pro',
+            feature: false,
+            confirmAddProject: true
+        },
+        {
+            name: 'Robot Gladiators',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+            languages: ['JavaScript'],
+            link: 'https://github.com/lernantino/robot-gladiators',
+            feature: false,
+            confirmAddProject: false
+        }
+    ]
+};
+  
+
+const pageHTML = generatePage(mockData);
+
+/*promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    
     });
+    */
--- src/page-template.js
+++ src/page-template.js
@@ -1,19 +1,43 @@
-const generatePage = (userName, gitHubName) => {
+module.exports = templateData => {
+  
+    // destructure projects and about data from templateData based on their property key names
+    //creates projects as an array of objects from templateData.projects
+    //and about as a big string of data from templateData.about
+    const { projects, about, ...header } = templateData;
+    // rest operator (...) takes the rest of the data that hasn't been accessed and puts it in
+    // a new object "header"
+    //Different than the spread operator!
+
     return `
     <!DOCTYPE html>
     <html lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Portfolio Demo</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
     </head>
+
     <body>
-        <h1>${userName}</h1>
-        <h2><a href="https://github.com/${gitHubName}">GitHub</a></h2>
+        <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+            <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+            <nav class="flex-row">
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
+            </nav>
+        </div>
+        </header>
+        <main class="container my-5">
+
+        </main>
+        <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+        </footer>
     </body>
     </html>
     `;
-};
-
-module.exports = generatePage;
+  };
